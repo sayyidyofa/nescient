002b350d6d78552a9ce210d6c23efc046b6c8760
Sayyid Y <sayyidyofa@gmail.com>
2021-03-03 18:28:29 +0700

    Types, utils

---
+++ src/types.ts
@@ -0,0 +1,1 @@
+export type SlidingData = Array<number>;
---
+++ src/utils/helpers.ts
@@ -0,0 +1,20 @@
+import config from "../config";
+import {SlidingData} from "../types";
+
+export function calculateVariance(data: SlidingData): number {
+    const squaredDiffToMean = (dataPoint: number, meanValue: number) => (dataPoint - meanValue) ** 2;
+    const mean = calculateMean(data);
+    return Math.sqrt(
+        data.reduce(
+            (previousValue, currentValue) =>
+                squaredDiffToMean(previousValue, mean) + squaredDiffToMean(currentValue, mean)
+        ) / ( config.windowSize - 1 )
+    );
+}
+
+export function calculateMean(data: SlidingData): number {
+    return data.reduce(
+        (previousValue, currentValue) =>
+            previousValue + currentValue
+    ) / config.windowSize;
+}
